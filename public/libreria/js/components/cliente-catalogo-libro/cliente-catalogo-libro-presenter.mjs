18117d2918b17e41137ddb0fd296b9f6e5175ab2
josemanuelmoraga <josemanuel.moraga1@alu.uclm.es>
2025-10-30 19:04:25 +0100

    Cambios para mostrar todos los atributos de libro

--- public/libreria/js/components/cliente-catalogo-libro/cliente-catalogo-libro-presenter.mjs
+++ public/libreria/js/components/cliente-catalogo-libro/cliente-catalogo-libro-presenter.mjs
@@ -5,17 +5,43 @@ export class ClienteCatalogoLibroPresenter extends Presenter {
     super(model, view, parentSelector);
   }
 
-  async refresh() {
-    let html = await this.getHTML();
-    this.parentElement.insertAdjacentHTML('beforeend', html);
+ async refresh() {
+  let html = await this.getHTML();
+  this.parentElement.insertAdjacentHTML('beforeend', html);
 
-    let node = this.parentElement.querySelector(`#titulo`);
-    node.setAttribute('id', `titulo_${this.model._id}`);
-    node.innerHTML = this.model.titulo;
+  // --- Título ---
+  let node = this.parentElement.querySelector(`#titulo`);
+  node.setAttribute('id', `titulo_${this.model._id}`);
+  node.innerHTML = this.model.titulo;
 
-    node = this.parentElement.querySelector(`#verLink`);
-    node.setAttribute('id', `verLink_${this.model._id}`);
-    node.setAttribute('href', `cliente-ver-libro.html?id=${this.model._id}`);
-    this.attachAnchors();
-  }
+  // --- Portada (¡Descomentada y corregida!) ---
+  node = this.parentElement.querySelector(`#portada`);
+  node.setAttribute('id', `portada_${this.model._id}`);
+  node.innerHTML = this.model.portada;
+
+  //Autores
+  node = this.parentElement.querySelector(`#autores`);
+  node.setAttribute('id', `autores_${this.model._id}`);
+  node.innerHTML = this.model.autores; 
+
+  //Precio 
+  node = this.parentElement.querySelector(`#precio`);
+  node.setAttribute('id', `precio_${this.model._id}`);
+  // Formateamos el precio como en la imagen
+  node.innerHTML = `€ ${this.model.precio}`; 
+
+  // ISBN 
+  node = this.parentElement.querySelector(`#isbn`);
+  node.setAttribute('id', `isbn_${this.model._id}`);
+  node.innerHTML = this.model.isbn;
+
+  // --- Ver Link ---
+  node = this.parentElement.querySelector(`#verLink`);
+  node.setAttribute('id', `verLink_${this.model._id}`);
+  node.setAttribute('href', `cliente-ver-libro.html?id=${this.model._id}`);
+
+  
+
+  this.attachAnchors();
+}
 }
