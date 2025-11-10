9b1df4f49571f9b8ac9254e034a56de195927b15
CarlosRubM <carlos.rubio9@alu.uclm.es>
2025-11-10 17:57:02 +0100

    Se comienza a realizar el proxy del cliente y se modifica model por proxy en el main

--- public/libreria/js/main.mjs
+++ public/libreria/js/main.mjs
@@ -1,5 +1,5 @@
 
-import { model } from "./model/model.mjs";
+import { proxy } from "./model/proxy.mjs"; //se cambia model por proxy
 import { router } from "./commons/router.mjs";
 import { InvitadoHomePresenter } from "./components/invitado-home/invitado-home-presenter.mjs";
 import { InvitadoVerLibroPresenter } from "./components/invitado-ver-libro/invitado-ver-libro-presenter.mjs";
@@ -24,32 +24,32 @@ import { AdminModificarLibroPresenter } from "./components/admin-modificar-libro
 export function init() {
   seed();
   // console.log(model)
-  router.register(/^\/libreria\/index.html$/, new InvitadoHomePresenter(model, 'invitado-home'));
-  router.register(/^\/libreria\/catalogo.html$/, new InvitadoHomePresenter(model, 'invitado-home'));
-  router.register(/^\/libreria\/invitado-ver-libro.html/, new InvitadoVerLibroPresenter(model, 'invitado-ver-libro'));
+  router.register(/^\/libreria\/index.html$/, new InvitadoHomePresenter(proxy, 'invitado-home'));
+  router.register(/^\/libreria\/catalogo.html$/, new InvitadoHomePresenter(proxy, 'invitado-home'));
+  router.register(/^\/libreria\/invitado-ver-libro.html/, new InvitadoVerLibroPresenter(proxy, 'invitado-ver-libro'));
   // router.register(/^\/libreria\/home.html$/, new HomePresenter(model, 'home'));
   //router.register(/^\/libreria$/, new HomePresenter(model, 'home'));
-  router.register(/^\/libreria$/, new InvitadoHomePresenter(model, 'invitado-home'));
+  router.register(/^\/libreria$/, new InvitadoHomePresenter(proxy, 'invitado-home'));
 
    //se añade el de registro de invitado
-  router.register(/^\/libreria\/invitado-registro.html$/, new InvitadoRegistroPresenter(model, 'invitado-registro'));
-  router.register(/^\/libreria\/invitado-ingreso.html$/, new InvitadoIngresoPresenter(model, 'invitado-ingreso'));
+  router.register(/^\/libreria\/invitado-registro.html$/, new InvitadoRegistroPresenter(proxy, 'invitado-registro'));
+  router.register(/^\/libreria\/invitado-ingreso.html$/, new InvitadoIngresoPresenter(proxy, 'invitado-ingreso'));
 
   //CLIENTE
-  router.register(/^\/libreria\/cliente-home.html$/, new ClienteHomePresenter(model, 'cliente-home'));
-  router.register(/^\/libreria\/cliente-ver-libro.html/, new ClienteVerLibroPresenter(model, 'cliente-ver-libro'));
-  router.register(/^\/libreria\/cliente-perfil.html/, new ClientePerfilPresenter(model, 'cliente-perfil'));
-  router.register(/^\/libreria\/cliente-carro.html/, new ClienteCarroPresenter(model, 'cliente-carro'));
-  router.register(/^\/libreria\/cliente-comprar-carro.html/, new ClienteComprarCarroPresenter(model, 'cliente-comprar-carro'));
-  router.register(/^\/libreria\/cliente-lista-compras.html/, new ClienteListaComprasPresenter(model, 'cliente-lista-compras'));
-  router.register(/^\/libreria\/cliente-ver-compra.html/, new ClienteVerCompraPresenter(model, 'cliente-ver-compra'));
+  router.register(/^\/libreria\/cliente-home.html$/, new ClienteHomePresenter(proxy, 'cliente-home'));
+  router.register(/^\/libreria\/cliente-ver-libro.html/, new ClienteVerLibroPresenter(proxy, 'cliente-ver-libro'));
+  router.register(/^\/libreria\/cliente-perfil.html/, new ClientePerfilPresenter(proxy, 'cliente-perfil'));
+  router.register(/^\/libreria\/cliente-carro.html/, new ClienteCarroPresenter(proxy, 'cliente-carro'));
+  router.register(/^\/libreria\/cliente-comprar-carro.html/, new ClienteComprarCarroPresenter(proxy, 'cliente-comprar-carro'));
+  router.register(/^\/libreria\/cliente-lista-compras.html/, new ClienteListaComprasPresenter(proxy, 'cliente-lista-compras'));
+  router.register(/^\/libreria\/cliente-ver-compra.html/, new ClienteVerCompraPresenter(proxy, 'cliente-ver-compra'));
   // router.register(/^\/libreria\/agregar-libro.html$/, new AgregarLibroPresenter(model, 'agregar-libro'));
 
   //ADMIN
-  router.register(/^\/libreria\/admin-home.html$/, new AdminHomePresenter(model, 'admin-home'));
-  router.register(/^\/libreria\/admin-ver-libro.html/, new AdminVerLibroPresenter(model, 'admin-ver-libro'));
-  router.register(/^\/libreria\/admin-perfil.html/, new AdminPerfilPresenter(model, 'admin-perfil'));
-  router.register(/^\/libreria\/admin-agregar-libro.html$/, new AdminAgregarLibroPresenter(model, "admin-agregar-libro"));
-  router.register(/^\/libreria\/admin-modificar-libro.html/, new AdminModificarLibroPresenter(model, "admin-modificar-libro"));
+  router.register(/^\/libreria\/admin-home.html$/, new AdminHomePresenter(proxy, 'admin-home'));
+  router.register(/^\/libreria\/admin-ver-libro.html/, new AdminVerLibroPresenter(proxy, 'admin-ver-libro'));
+  router.register(/^\/libreria\/admin-perfil.html/, new AdminPerfilPresenter(proxy, 'admin-perfil'));
+  router.register(/^\/libreria\/admin-agregar-libro.html$/, new AdminAgregarLibroPresenter(proxy, "admin-agregar-libro"));
+  router.register(/^\/libreria\/admin-modificar-libro.html/, new AdminModificarLibroPresenter(proxy, "admin-modificar-libro"));
   router.handleLocation();
 }
---
+++ public/libreria/js/model/proxy.mjs
@@ -0,0 +1,194 @@
+export const ROL = {
+  ADMIN: "ADMIN",
+  CLIENTE: "CLIENTE",
+};
+
+
+export class LibreriaProxy {
+
+  constructor() { }
+
+  /**
+   * Libros
+   */
+
+  async getLibros() {
+    let response = await fetch('http://localhost:3000/api/libros');
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  async addLibro(obj) {
+    let response = await fetch('http://localhost:3000/api/libros', {
+      method: 'POST',
+      body: JSON.stringify(obj),
+      headers: { 'Content-Type': 'application/json;charset=utf-8' }
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  async setLibros(array) {
+    let response = await fetch('http://localhost:3000/api/libros', {
+      method: 'PUT',
+      body: JSON.stringify(array),
+      headers: { 'Content-Type': 'application/json;charset=utf-8' }
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  async getLibroPorId(id) {
+    let response = await fetch(`http://localhost:3000/api/libros/${id}`);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  
+  async getLibroPorIsbn(id) {
+    let response = await fetch(`http://localhost:3000/api/libros?isbn=${isbn}`);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  async getLibroPorTitulo(titulo){
+    let response = await fetch(`http://localhost:3000/libros?titulo=${encodeURIComponent(titulo)}`)
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+
+  async removeLibro(id) {
+    let response = await fetch(`http://localhost:3000/api/libros/${id}`, { method: 'DELETE' });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  async updateLibro(obj) {
+    let response = await fetch(`http://localhost:3000/api/libros/${obj._id}`, { 
+      method: 'PUT',
+      body: JSON.stringify(obj),
+      headers: { 'Content-Type': 'application/json;charset=utf-8' }
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+    /**
+   * Clientes
+   */
+
+
+  async addCliente(obj){
+    let response = await fetch('http://localhost:3000/api/clientes', {
+      method: 'POST',
+      body: JSON.stringify(obj),
+      headers: { 'Content-Type': 'application/json;charset=utf-8' }
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  } 
+
+  async getClientes(){
+    let response = await fetch('http://localhost:3000/api/clientes');
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  async getClientePorId(_id) {
+    let response = await fetch(`http://localhost:3000/api/usuarios/${_id}`);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+  async getClientePorEmail(email){
+    let response = await fetch(`http://localhost:3000/clientes?email=${encodeURIComponent(email)}`)
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+   async getClientePorDni(dni){
+    let response = await fetch(`http://localhost:3000/clientes?dni=${encodeURIComponent(dni)}`)
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  async removeCliente(id) {
+    let response = await fetch(`http://localhost:3000/api/clientes/${id}`, { method: 'DELETE' });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  async setClientes(array) {
+    let response = await fetch('http://localhost:3000/api/clientes', {
+      method: 'PUT',
+      body: JSON.stringify(array),
+      headers: { 'Content-Type': 'application/json;charset=utf-8' }
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  async updateCliente(obj) {
+    let response = await fetch(`http://localhost:3000/api/clientes/${obj._id}`, { 
+      method: 'PUT',
+      body: JSON.stringify(obj),
+      headers: { 'Content-Type': 'application/json;charset=utf-8' }
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+  }
+
+  
+
+
+}
+
+export const proxy = new LibreriaProxy();
